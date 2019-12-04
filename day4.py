7483d8e5a577cc7f845d23a5a96ffdde5c38e0ea
Rikard Eriksen <rikard.s.eriksen@gmail.com>
2019-12-04 18:39:27 +0100

    add day 4 part 2

--- day4.py
+++ day4.py
@@ -1,5 +1,6 @@
 import re
 double_pattern = re.compile(r"([0-9])\1")
+double_nothree = re.compile(r"([0-9])(\1+)")
 
 password_range = range(264793, 803935+1)
 
@@ -7,7 +8,17 @@ def has_double(number:int) -> bool:
   r = double_pattern.search(str(number))
   return False if r is None else True
 
+def has_double_stricter(number: int) -> bool:
+  r = double_nothree.findall(str(number))
+  if not r:
+    return False
+  for _, match in r:
+    if len(match) == 1:
+      return True
+  return False
+
 def is_increasing(number:int) -> bool:
   return str(number) == "".join(sorted(str(number)))
 
-print(len([p for p in password_range if all((has_double(p), is_increasing(p)))]))
+print(len([p for p in password_range if all((has_double(p), is_increasing(p)))]))  # part 1
+print(len([p for p in password_range if all((has_double_stricter(p), is_increasing(p)))]))  # part 2
