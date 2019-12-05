88d3e01aff5901fadc0a73ff8beed1b1dfc6d325
Rikard Eriksen <rikard.s.eriksen@gmail.com>
2019-12-05 07:56:20 +0100

    add day 5 part 2

--- day5.py
+++ day5.py
@@ -12,23 +12,43 @@ def decode_opcode(opcode:int) -> tuple:
   return op, parmm1, parmm2, parmm3
 
 def run_to_halt(program):
+  increase_pc = True
   pc = 0
   opcode = program[pc]
   while opcode != 99:
     op, pm1, pm2, pm3 = decode_opcode(opcode)
+
+    def i1(): return program[pc+1] if pm1 == 0 else pc+1
+    def i2(): return program[pc+2] if pm2 == 0 else pc+2
+    def i3(): return program[pc+3] if pm3 == 0 else pc+3
+
     if op == 1:
-      program[program[pc+3] if pm3 == 0 else pc+3] = (program[program[pc+1] if pm1 == 0 else pc+1]) + (program[program[pc+2] if pm2 == 0 else pc+2])
+      program[i3()] = program[i1()] + program[i2()]
     elif op == 2:
-      program[program[pc+3] if pm3 == 0 else pc+3] = (program[program[pc+1] if pm1 == 0 else pc+1]) * (program[program[pc+2] if pm2 == 0 else pc+2])
+      program[i3()] = program[i1()] * program[i2()]
     elif op == 3:
       assert(pm1 == 0), "parameter mode is not positional for first param"
       num = int(input("awaiting input >>> "))
-      program[program[pc+1]] = num
+      program[i1()] = num
     elif op == 4:
-      print(program[program[pc+1] if pm3 == 0 else pc+1])
+      print(program[i1()])
+    elif op == 5:
+      if program[i1()] != 0:
+        pc = program[i2()]
+        increase_pc = False
+    elif op == 6:
+      if program[i1()] == 0:
+        pc = program[i2()]
+        increase_pc = False
+    elif op == 7:
+      program[i3()] = 1 if program[i1()] < program[i2()] else 0
+    elif op == 8:
+      program[i3()] = 1 if program[i1()] == program[i2()] else 0
     else:
       raise ValueError(f"Unknown opcode {op} from {opcode} at position {pc}")
-    pc += {1: 4, 2: 4, 3: 2, 4: 2}.get(op)
+    if increase_pc:
+      pc += {1: 4, 2: 4, 3: 2, 4: 2, 5:3, 6: 3, 7: 4, 8:4}.get(op, -1)
+    increase_pc = True
     opcode = program[pc]
   return program[0]
 
