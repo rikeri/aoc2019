5f5c8905abbc2efa4e0a1917785f301cafb08055
Rikard Eriksen <rikard.s.eriksen@gmail.com>
2019-12-08 01:05:07 +0100

    add day 7 part 2 (sacrificing part 1)

--- day7.py
+++ day7.py
@@ -13,63 +13,83 @@ def decode_opcode(opcode:int) -> tuple:
   parmms, parmm3 = divmod(parmms, 10)
   return op, parmm1, parmm2, parmm3
 
-def run_to_halt(program, amplifier_index=0):
-  increase_pc = True
-  pc = 0
-  opcode = program[pc]
-  got_phase = False
-  got_input = False
-  while opcode != 99:
-    op, pm1, pm2, pm3 = decode_opcode(opcode)
+class Amplifier():
+  def __init__(self, program, amplifier_index):
+    self.amplifier_index = amplifier_index
+    self.program = program
+    self.increase_pc = True
+    self.got_phase = False
+    self.pc = 0
+    self.halted = False
 
-    def i1(): return program[pc+1] if pm1 == 0 else pc+1
-    def i2(): return program[pc+2] if pm2 == 0 else pc+2
-    def i3(): return program[pc+3] if pm3 == 0 else pc+3
+  def __str__(self):
+    name = {0: "A", 1: "B", 2: "C", 3: "D", 4: "E"}.get(self.amplifier_index)
+    return f"{name} pc:{self.pc}, halted:{self.halted}, gp:{self.got_phase}, ioputs:{ioputs}"
 
-    if op == 1:
-      program[i3()] = program[i1()] + program[i2()]
-    elif op == 2:
-      program[i3()] = program[i1()] * program[i2()]
-    elif op == 3:
-      assert(pm1 == 0), "parameter mode is not positional for first param"
-      if not got_phase:
-        got_phase = True
-        num = phases[amplifier_index]
-      elif not got_input:
-        got_input = True
-        num = ioputs[amplifier_index]
-      program[i1()] = num
-    elif op == 4:
-      ioputs[amplifier_index + 1] = program[i1()]
-    elif op == 5:
-      if program[i1()] != 0:
-        pc = program[i2()]
-        increase_pc = False
-    elif op == 6:
-      if program[i1()] == 0:
-        pc = program[i2()]
-        increase_pc = False
-    elif op == 7:
-      program[i3()] = 1 if program[i1()] < program[i2()] else 0
-    elif op == 8:
-      program[i3()] = 1 if program[i1()] == program[i2()] else 0
-    else:
-      raise ValueError(f"Unknown opcode {op} from {opcode} at position {pc}")
-    if increase_pc:
-      pc += {1: 4, 2: 4, 3: 2, 4: 2, 5:3, 6: 3, 7: 4, 8:4}.get(op, -1)
-    increase_pc = True
-    opcode = program[pc]
-  return program[0]
+  def run_to_output(self):
+    if self.halted:
+      return
+    program = self.program
+    amp_i = self.amplifier_index
+    opcode = program[self.pc]
+
+    got_input = False
+    return_after_pc = False
+    while opcode != 99:
+      op, pm1, pm2, pm3 = decode_opcode(opcode)
+
+      def i1(): return program[self.pc+1] if pm1 == 0 else self.pc+1
+      def i2(): return program[self.pc+2] if pm2 == 0 else self.pc+2
+      def i3(): return program[self.pc+3] if pm3 == 0 else self.pc+3
+
+      if op == 1:
+        program[i3()] = program[i1()] + program[i2()]
+      elif op == 2:
+        program[i3()] = program[i1()] * program[i2()]
+      elif op == 3:
+        assert(pm1 == 0), "parameter mode is not positional for first param"
+        if not self.got_phase:
+          self.got_phase = True
+          num = phases[amp_i]
+        else:
+          num = ioputs[amp_i]
+        program[i1()] = num
+      elif op == 4:
+        ioputs[(amp_i + 1)%5] = program[i1()]
+        return_after_pc = True
+      elif op == 5:
+        if program[i1()] != 0:
+          self.pc = program[i2()]
+          self.increase_pc = False
+      elif op == 6:
+        if program[i1()] == 0:
+          self.pc = program[i2()]
+          self.increase_pc = False
+      elif op == 7:
+        program[i3()] = 1 if program[i1()] < program[i2()] else 0
+      elif op == 8:
+        program[i3()] = 1 if program[i1()] == program[i2()] else 0
+      else:
+        raise ValueError(f"Unknown opcode {op} from {opcode} at position {self.pc}")
+      if self.increase_pc:
+        self.pc += {1: 4, 2: 4, 3: 2, 4: 2, 5:3, 6: 3, 7: 4, 8:4}.get(op, -1)
+      self.increase_pc = True
+      if return_after_pc:
+        return
+      opcode = program[self.pc]
+    self.halted = True
 
 base_program = parse(tx)
 max_output = 0
 max_phases = None
-for phases in permutations(range(5)):
-  ioputs = [0,0,0,0,0,0]
-  for amp_i in range(5):
-    run_to_halt(base_program.copy(), amp_i)
+for phases in permutations(range(5, 10)):
+  ioputs = [0,0,0,0,0]  # connection between machines
+  amplifiers = [Amplifier(base_program.copy(), amp_i) for amp_i in range(5)]
+  while not all(a.halted for a in amplifiers):
+    for a in amplifiers:
+      a.run_to_output()
   if ioputs[-1] > max_output:
-    max_output = ioputs[-1]
+    max_output = ioputs[0]
     max_phases = phases
 print(f"Found max output {max_output} with the phase combination {max_phases}")
   
