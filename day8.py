764de818ab49a070d13139ca625fbd98052fe6a6
Rikard Eriksen <rikard.s.eriksen@gmail.com>
2019-12-08 23:44:14 +0100

    add day 8 part 2

--- day8.py
+++ day8.py
@@ -4,7 +4,6 @@ with open("input8.txt", "r") as f:
 height = 6
 width = 25
 area = height*width
-# image = "123456789012"
 num_layers = len(image) // area
 def countof(digit, string): return len([c for c in string if c == digit])
 
@@ -13,8 +12,19 @@ for i in range(0, len(image)-1, area):
   layer = image[i:i+area]
   zerocount = countof("0", layer)
   layers.append((zerocount, layer))
-layers.sort()
-layer = layers[0][1]
+layer = sorted(layers)[0][1]
 onecount = countof("1", layer)
 twocount = countof("2", layer)
 print(onecount * twocount) # puzzle 1
+
+render = ["2"]*area
+for i in range(area):
+  for _, l in layers:
+    if render[i] != "2" or l[i] == "2":
+      continue
+    render[i] = l[i]
+
+raster = ["#" if c == "1" else " " for c in render]
+
+for i in range(0, area, width):
+  print("".join(raster[i:i+width])) # puzzle 2
